bbb20a412d14d9dbd4de2a086466d8f65caa55d4
Diego Lopez <diegolopezlatour@gmail.com>
2023-04-08 16:21:56 -0400

    react-router-dom for login page

--- restaurant-app/src/App.js
+++ restaurant-app/src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import MainContent from "./components/MainContent";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
 import "./App.css";
+
 export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
   return (
     <>
       <Header position="absolute" />
